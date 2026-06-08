7c314126da461f3a1e99ac46a3857dd72b556ffa
Kiranrohith <kiranrohith246@gmail.com>
2026-06-08 17:56:14 +0530

    proper folder restructuring

--- Day2/hello.py
+++
@@ -1,9 +0,0 @@
-#This is a print statement
-print("Hello, World")
-
-#declaration of variable & assigning values to it
-num1 = 10
-num2 = 5
-num3 = num1/num2 #Division operation
-print(num3) #printing the answer for the division operation
-
