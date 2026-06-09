03e492e49d49052fd33f2a978ba7f5e45933a97f
Kiranrohith <kiranrohith246@gmail.com>
2026-06-09 10:01:44 +0530

    Added comments

--- day_2/hello.py
+++ day_2/hello.py
@@ -1,6 +1,7 @@
-print("Hello, world!!")
+print("Hello, world!!") #This is print functiion
 
+#declaring variables & assigning values to it
 name = "Kiran"
 age = 22
 is_intern = True
-print(f"I am {name}, my age is {age}, I am an intern: {is_intern}")
+print(f"I am {name}, my age is {age}, I am an intern: {is_intern}") #printing the variables
