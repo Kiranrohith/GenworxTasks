f596e26f24a11278fbdee90ea2470b879eae8def
Kiranrohith <kiranrohith246@gmail.com>
2026-06-15 12:35:34 +0530

    week-1 task compilation

---
+++ week_1/day_2/hello.py
@@ -0,0 +1,7 @@
+print("Hello, world!!") #This is print functiion
+
+#declaring variables & assigning values to it
+name = "Kiran"
+age = 22
+is_intern = True
+print(f"I am {name}, my age is {age}, I am an intern: {is_intern}") #printing the variables
