198c413ce29140968592f51ce9dc22b117300c43
Kiranrohith <kiranrohith246@gmail.com>
2026-07-06 18:30:01 +0530

    Add React components, props, and state

---
+++ week_4/day_19_tictactoe/tictactoe/src/App.tsx
@@ -0,0 +1,16 @@
+/*
+  Task: Tictactoe
+  components: square
+  (square, board, game)
+*/
+
+import Game from "./components/square";
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+
+export default App;
